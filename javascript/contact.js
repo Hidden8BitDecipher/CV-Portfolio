879afc4d38d567d2d38e8d283ad7168b920addc8
Hidden8BitDecipher <sigmatv45@gmail.com>
2024-07-25 13:10:11 -0800

    Add files via upload

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,1 +1,13 @@
+function confirmSubmit() {
+    
+    var confirmed = confirm("Weet u zeker dat dit uw juiste Email is?");
 
+    
+    if (confirmed) {
+      
+      console.log("");
+    } else {
+      
+      console.log("Formulier verzending geannuleerd.");
+    }
+  }
